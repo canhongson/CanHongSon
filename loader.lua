b090b69f58bf9c0df8ce33bb7f0254056255608e
CanHongSon <cans27843@gmail.com>
2026-02-21 17:31:07 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ local Scripts = {
     [78466992256287] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/ReignPiece",
     [77747658251236] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
     [75159314259063] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
-    [99684056491472] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece"
+    [99684056491472] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
     [101640913672688] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/AnimeGhost"
 }
 
