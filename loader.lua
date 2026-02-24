80c9d6c734d6417f8500934ca8577c8374435193
CanHongSon <cans27843@gmail.com>
2026-02-24 12:39:11 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,8 @@ local Scripts = {
     [78466992256287] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/ReignPiece",
     [77747658251236] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
     [75159314259063] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
-    [99684056491472] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece"
+    [99684056491472] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
+    [123955125827131] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece"
 }
 
 local url = Scripts[game.PlaceId]
