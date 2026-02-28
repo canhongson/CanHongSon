5f74e2ba1d969b6a33f5f67300451c2d0a062028
CanHongSon <cans27843@gmail.com>
2026-02-28 12:55:41 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,9 @@
-local Scripts = {
+local Script = {
     [9338091695] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/ReignPiece",
     [9186719164] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
 }
 
-local url = Scripts[game.GameId]
+local url = Script[game.GameId]
 
 if url then
     loadstring(game:HttpGet(url))()
