ca88868ae0d7309e8f1b215bbe55d1a464d2184a
CanHongSon <cans27843@gmail.com>
2026-04-29 23:57:07 +0700

    Add PiratePiece URL to loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 local Script = {
     [9338091695] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/ReignPiece",
     [9186719164] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
+    [9917246399] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/PiratePiece",
 }
 
 local url = Script[game.GameId]
