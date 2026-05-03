36b685894a15c295786e44f3f945a59c7e742ad5
CanHongSon <cans27843@gmail.com>
2026-05-03 08:39:22 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local Script = {
     [9338091695] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/ReignPiece",
     [9186719164] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/SailorPiece",
     [9917246399] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/PiratePiece",
+    [9382839773] = "https://raw.githubusercontent.com/canhongson/CanHongSon/refs/heads/main/LineagePiece",
 }
 
 local url = Script[game.GameId]
